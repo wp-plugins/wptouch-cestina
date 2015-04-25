b2179e5f825315f235307a5123c281b8724e6027
expres-web <expres-web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-25 01:24:30 +0000

    Verze 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wptouch-cestina/trunk@1145311 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== WPtouch čeština čeština ===
+Contributors: expres-web
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7F53XKXAB2HSG
+Tags: WPtouch, čeština, WPtouch čeština, WPtouch Mobile Plugin, WPtouch Mobile Plugin čeština
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Přeloží plugin WPtouch Mobile Plugin do češtiny.
+
+
+== Installation ==
+
+Instalace je velmi jednoduchá
+
+1. Nainstalujte plugin přes administraci v Pluginy.
+1. Activujte plugin.
+
+== Screenshots ==
+
+1. Náhled
+
+== Frequently Asked Questions ==
+
+Našel jsem chybu, co mám dělat?
+
+Pokud naleznete chybu, zdělte nám to prosím
+
+== Changelog ==
+
+= 1.0 =
+Spustění a zveřejnění pluginu.
---
+++ wptouch-cestina.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Plugin Name: WPtouch čeština
+Plugin URI: http://expres-web.cz
+Description: Přeloží plugin WPtouch do češtiny.
+Version: 1.0
+Author: Expres-Web.cz
+Author URI: http://expres-web.cz
+Text Domain: wprc
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Maybe load the WPtouch čeština.
+ *
+ * @since 1.0
+ */
+function wprc_load_textdomain() {
+	$mofile = WP_PLUGIN_DIR . '/wptouch-cestina/jazyky/' . apply_filters( 'wptouch-pro_locale', get_locale() ) . '.mo';
+
+	if ( file_exists( $mofile ) )
+		return load_textdomain( 'wptouch-pro', $mofile );
+}
+add_action( 'wp_loaded', 'wprc_load_textdomain' );
